77233447941c365a5c3721921913bfa417d3c40d
tomfyfe <tomfyfe85@gmail.com>
2025-09-20 12:41:40 +0100

    feat: create navbar component with options for background colour variation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import Navbar from "./components/NavBar";
+
 function App() {
-  return <div className="p-8 text-blue-500 text-2xl">Tailwind Test</div>;
+  return (
+    <>
+      <div className="p-8 text-blue-500 text-2xl">Tailwind Test</div>;
+      <Navbar variant="transparent" />
+    </>
+  );
 }
 export default App;
