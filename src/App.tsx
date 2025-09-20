750e9af4ab12f5370495d3b6baae0152f6bb7d85
tomfyfe <tomfyfe85@gmail.com>
2025-09-20 15:32:51 +0100

    feat: add DNS logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Navbar from "./components/NavBar";
 function App() {
   return (
     <>
-      <div className="p-8 text-blue-500 text-2xl">Tailwind Test</div>;
-      <Navbar variant="transparent" />
+      <Navbar variant="purple700" />
     </>
   );
 }
