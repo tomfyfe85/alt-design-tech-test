a4a281a44b78c762ca057d1b294a1768b5e2ddc5
tomfyfe <tomfyfe85@gmail.com>
2025-09-21 09:32:06 +0100

    feat: add We make your IT work easy. h1's

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,36 @@ export default function Hero() {
         className="w-[1512px] h-[742px] absolute top-[1px] left-[-8px]"
         style={{ backgroundColor: "#31297D" }}
       >
-        {/* Content will go here later */}
+        <h1
+          className="absolute top-[249px] left-[133px] text-white text-[70px]"
+          style={{
+            fontFamily: "Inter",
+            fontWeight: 700,
+            lineHeight: "100%",
+            letterSpacing: "0%",
+            width: "463px",
+            height: "85px",
+            top: "249px",
+            left: "133px",
+            whiteSpace: "nowrap",
+          }}
+        >
+          We make your
+        </h1>
+        <h1
+          className="absolute top-[318px] left-[133px] text-white text-[70px]"
+          style={{
+            fontFamily: "Inter",
+            fontWeight: 700,
+            lineHeight: "100%",
+            letterSpacing: "0%",
+            width: "412px",
+            height: "85px",
+            whiteSpace: "nowrap",
+          }}
+        >
+          IT work easy.
+        </h1>
       </section>
     </div>
   );
