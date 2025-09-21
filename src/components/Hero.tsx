434ec94a62056bd72fbc549d351f3259782bbd87
tomfyfe <tomfyfe85@gmail.com>
2025-09-21 09:09:38 +0100

    feat: add hero rectangle

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,12 @@
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <section
+        className="w-[1512px] h-[742px] absolute top-[1px] left-[-8px]"
+        style={{ backgroundColor: "#31297D" }}
+      >
+        {/* Content will go here later */}
+      </section>
+    </div>
+  );
 }
