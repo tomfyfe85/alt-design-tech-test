aeec4fab28c97a926ddd0d16e444f6f05ea4d499
tomfyfe <tomfyfe85@gmail.com>
2025-09-21 11:45:11 +0100

    feat: add service cards component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import LandingPage from "../pages/LandingPage";
+import ServiceCards from "./components/ServiceCards";
 
 function App() {
   return (
     <div className="relative">
       <Navbar variant="transparent" />
       <LandingPage />
+      <ServiceCards />
     </div>
   );
 }
---
+++ src/components/ServiceCards.tsx
@@ -0,0 +1,3 @@
+export default function ServiceCards() {
+  return <div>ServiceCards</div>;
+}
