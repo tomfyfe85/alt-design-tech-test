255577a9c9d5dcff0db08792bdfc3fe80a638aa5
tomfyfe <tomfyfe85@gmail.com>
2025-09-23 01:31:45 +0100

    feat: footer componet is in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import LandingPage from "../pages/LandingPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="relative">
       <Navbar variant="transparent" />
       <LandingPage />
+      <Footer variant="transparent" />
     </div>
   );
 }
