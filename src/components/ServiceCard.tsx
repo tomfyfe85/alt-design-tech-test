56f01b96164bb1edc2641dab139f76317474b407
tomfyfe <tomfyfe85@gmail.com>
2025-09-25 12:18:19 +0100

    edit: cards  are correct shape

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -15,7 +15,7 @@ export default function ServiceCard({
   learnMoreLink,
 }: ServiceCardProps) {
   return (
-    <div className="bg-white hover:bg-[#F4207B] text-gray-800 hover:text-white rounded-[10px] p-6 shadow-lg transition-all duration-150 group cursor-pointer hover:-translate-y-[10px] w-[600px] h-[610px] flex flex-col items-center">
+    <div className="bg-white hover:bg-[#F4207B] text-gray-800 hover:text-white rounded-[20px] p-6 shadow-lg transition-all duration-150 group cursor-pointer hover:-translate-y-[10px] w-[570px] h-[690px] flex flex-col items-center">
       {/* Icon Container */}
       <div className="relative w-[1105px] h-[90px] mt-[75px] mb-8">
         <img
